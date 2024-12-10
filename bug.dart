8830d03ed7012b988da3ffeb6ecbcedf8174b5c7
BugHunter-X <askzenai@gmail.com>
2024-12-10 07:28:06 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,20 @@
+```dart
+import 'dart:async';
+
+Future<void> main() async {
+  final completer = Completer<int>();
+
+  // Simulate an asynchronous operation that might complete or throw an exception.
+  Timer(const Duration(seconds: 2), () {
+    //completer.complete(10);
+    completer.completeError(Exception('Something went wrong!'));
+  });
+
+  try {
+    final result = await completer.future;
+    print('Result: $result');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+```
